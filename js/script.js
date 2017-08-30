2234811ddaaef8dc7a564166ac5d4cc3af69fa25
Chang Shu-huai <junsuwhy@gmail.com>
2017-08-30 22:06:51 +0800

    Add files.

---
+++ js/script.js
@@ -0,0 +1,251 @@
+var all_questions=[
+
+    
+{"title":"當你在家中廚房打開魚罐頭時，在其他房間的貓咪聽到開罐聲音後會…",options:[
+{"name":"a","label":"跑到床/椅下躲起來"},
+{"name":"b","label":"快速的抬起頭來，但懶得起身，繼續呼呼大睡"},
+{"name":"c","label":"不管在哪都會迅速跑到廚房"}
+]},
+{"title":"當你把寵物提籃拿出來時，貓咪會…",options:[
+{"name":"a","label":"衝衝一瞥"},
+{"name":"b","label":"跑掉，但如果有給予零食就會跑回來"},
+{"name":"c","label":"躲起來，說甚麼都不出來"}
+]},
+{"title":"貓咪多常為了吃東西而把熟睡中你的你吵醒",options:[
+{"name":"a","label":"從未"},
+{"name":"b","label":"偶而"},
+{"name":"c","label":"每天"}
+]},
+{"title":"家中貓咪曾經因為看過你做，所以學會做某些事情嗎？例如有些貓咪從主人身上學會如何開門或是打開櫥櫃，甚至會開關燈",options:[
+{"name":"a","label":"從未"},
+{"name":"b","label":"有，貓咪從我身上學會一、兩個技能，例如開櫥櫃"},
+{"name":"c","label":"有，有很多事情都是貓咪藉由觀察我學會的"}
+]},
+{"title":"如果你每天都固定時間回家，家中貓咪是否會意識到你回來的時間，是先在門口等待?",options:[
+{"name":"a","label":"從未"},
+{"name":"b","label":"有，貓咪通常在門口等我"},
+{"name":"c","label":"有，貓咪除了在門口等我，還會一路跟隨我直到我走進廚房準備貓咪的食物"}
+]},
+{"title":"如果貓咪的玩具或是食物掉落/卡在物體(如冰箱)後面，貓咪會…",options:[
+{"name":"a","label":"嘗試撈一兩次，若撈不到就放棄"},
+{"name":"b","label":"嘗試用嘴巴叼回及爪子撈，一段時間後放棄"},
+{"name":"c","label":"嘗試用嘴巴叼回及爪子撈直到撈到為止，否則永不放棄"}
+]},
+{"title":"你的貓咪喜歡跟你玩遊戲嗎?",options:[
+{"name":"a","label":"不盡然"},
+{"name":"b","label":"偶爾"},
+{"name":"c","label":"非常喜歡，尤其是刺激或是挑戰性夠的"}
+]},
+{"title":"如果貓咪看到窗外的小鳥，他會…",options:[
+{"name":"a","label":"把臉貼在玻璃上，試圖靠近小鳥"},
+{"name":"b","label":"用爪子抓窗或是持續嚎叫，直到用百葉窗等東西阻擋他視線"},
+{"name":"c","label":"急著跑到大門口並哀求我讓他出門"}
+]},
+{"title":"如果你把食物或是貓砂盆移動到家中其他地方，貓咪需要多久才能適應新的位置",options:[
+{"name":"a","label":"幾個星期"},
+{"name":"b","label":"幾天~一星期"},
+{"name":"c","label":"一天內，甚至更短的時間"}
+]},
+{"title":"當你叫貓咪的名字，他會",options:[
+{"name":"a","label":"沒反應"},
+{"name":"b","label":"看看我或是把耳朵轉往我的方向"},
+{"name":"c","label":"立即跑來我身邊"}
+]},
+{"title":"你的貓咪是否厭倦你給他準備的玩具，而另外創造了自己喜歡的玩具，如你的襪子、魚缸、紙球等",options:[
+{"name":"a","label":"我的貓不喜歡玩玩具"},
+{"name":"b","label":"偶爾"},
+{"name":"c","label":"幾乎每天都有新玩意兒"}
+]},
+{"title":"如果家中曾經有位客人來作客時戲弄貓咪，讓貓咪厭煩生氣，之後當那位客人再度來家中時，貓咪是否會故意避開那人，僅給其他客人摟抱/撫摸呢？",options:[
+{"name":"a","label":"不會，我家貓咪對客人一視同仁"},
+{"name":"b","label":"不會，貓咪一直有特定喜好的人，但原因標準不清楚"},
+{"name":"c","label":"大部分會，我覺得我的貓很會計仇"}
+]},
+{"title":"你是否常常下班回到家才發現家裡的貓咪又趁你不在家時惡作劇了？",options:[
+{"name":"a","label":"從未"},
+{"name":"b","label":"偶而"},
+{"name":"c","label":"總是發生"}
+]},
+{"title":"當你撫摸你的貓咪，貓咪是否會讓你知道喜歡被撫摸哪邊或是撫摸多久？",options:[
+{"name":"a","label":"不會，他並不介意被撫摸多久"},
+{"name":"b","label":"如果摸到他喜歡的位置他會發出呼嚕呼嚕聲，若摸到他厭煩或是不想被摸的位置，他會發出嘶嘶聲制止你繼續撫摸"},
+{"name":"c","label":"摸的時候貓咪會自動換位置跟姿勢，讓你摸他想被撫摸的地方"}
+]},
+{"title":"當你想教貓咪一個新的把戲時，他的反應是？",options:[
+{"name":"a","label":"我的貓從不理會我教他的東西，沒有任何回應"},
+{"name":"b","label":"經過一段時間的訓練後，貓咪可以表現得很好"},
+{"name":"c","label":"在短時間內就學會新把戲，並表現得很好"}
+]},
+{"title":"若你已經教會貓咪新把戲，在沒有複習的情況下過了一個月後，再請貓咪演練時…",options:[
+{"name":"a","label":"很可能會失敗"},
+{"name":"b","label":"可能會成功"},
+{"name":"c","label":"絕對會成功，不可能失敗"}
+]},
+{"title":"當貓咪想要一個東西時，他會",options:[
+{"name":"a","label":"發出呼嚕聲撒嬌"},
+{"name":"b","label":"比平常喵喵叫的聲音大一點"},
+{"name":"c","label":"依照他喜愛的程度，發出許多不同的聲音嚎叫"}
+]},
+{"title":"貓咪找你陪他玩遊戲的頻率",options:[
+{"name":"a","label":"很少"},
+{"name":"b","label":"偶爾"},
+{"name":"c","label":"常常，幾乎每天"}
+]},
+{"title":"當貓砂盆需要清理時，貓咪有甚麼反應？",options:[
+{"name":"a","label":"一直往貓砂盆跑或是待在那邊，直到你注意到"},
+{"name":"b","label":"使用花盆等替代品上廁所或是嘗試跑到戶外上廁所"},
+{"name":"c","label":"直接拉在貓砂盆外面，並大聲嚎叫"}
+]},
+{"title":"如果碗中已經沒有貓食，貓咪會…",options:[
+{"name":"a","label":"靜靜待在食物碗盤附近，等人來餵食"},
+{"name":"b","label":"跳躍到腿上喵喵叫，直到你意識到要去餵食他"},
+{"name":"c","label":"嘗試打開櫥櫃，把飼料叼出來吃"}
+]},
+
+
+];
+
+for(var i = 0;i < all_questions.length;i++){
+//    all_questions[i].className = [];
+//    all_questions[i].getStyle = function(){
+//        var str = "";
+//        for(var j = 0;j < this.className.length;j++){
+//            str += " "+this.className[j];
+//        }
+//        if(typeof this.options == 'undefined'){
+//            str += " rule-section-title";
+//        }else{
+//            str += " each-rule";
+//        }
+//        return str;
+//    }
+}
+
+ var app = angular.module("page", []);
+    app.controller('MgCtrl',function(){
+        this.questions = all_questions;
+        angular.element(document).ready(function(){
+           init(); 
+        });
+//        this.addStyle=function(rule, style_value){
+//            console.log(rule);
+//            rule.className.push(style_value);
+//        }
+//        this.rmStyle=function(rule, style_value){
+//            for(var i = 0;i < rule.className.length;i++){
+//                if(rule.className[i]==style_value){
+//                    rule.className.splice(i);
+//                }
+//            }
+//        }
+    });
+
+
+
+
+
+function init(){
+    
+
+    // More info https://github.com/hakimel/reveal.js#configuration
+            Reveal.initialize({
+                controls: false,
+                progress: true,
+                history: true,
+                center: true,
+                transition: 'slide', // none/fade/slide/convex/concave/zoom
+                // More info https://github.com/hakimel/reveal.js#dependencies
+    //            dependencies: [
+    //                { src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
+    //                { src: 'plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+    //                { src: 'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+    //                { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+    //                { src: 'plugin/zoom-js/zoom.js', async: true },
+    //                { src: 'plugin/notes/notes.js', async: true }
+    //            ]
+            });
+
+        $(function(){
+            window.getAnswers = function(){
+                var rtnArr = [];
+                var total = 0;
+                $selections = $('[name^=question-]:checked');
+                $selections.each(function(){
+                    if($(this).val()=='a'){
+                        total--;
+                    }
+                    if($(this).val()=='c'){
+                        total++;
+                    }
+                })
+
+                return total;
+            }
+
+            window.setFinalAnswer = function(input_text){
+                $('.result').html(input_text);
+            }
+
+            $('input').click(function(){
+                var total = getAnswers(); 
+                setFinalAnswer(total);
+            });
+
+            $('.next').click(function(){
+                Reveal.right();
+            })
+
+
+
+            Reveal.slide(0);
+
+            $('input').click(function(){
+                setTimeout(function(){
+                  Reveal.right();  
+
+                },500);
+            })
+
+    //        $('.form-iframe').load(function() {
+    //            setTimeout(function(){
+    //              Reveal.right();  
+    //
+    //            },500);
+    //        });
+
+    //        Reveal.addEventListener( 'slidechanged', function( event ) {
+    //            window.href=""
+    //        }
+
+             window.fbAsyncInit = function() {
+                FB.init({
+                  appId            : '144111119374573',
+                  autoLogAppEvents : true,
+                  xfbml            : true,
+                  version          : 'v2.10'
+                });
+                FB.AppEvents.logPageView();
+              };
+
+              (function(d, s, id){
+                 var js, fjs = d.getElementsByTagName(s)[0];
+                 if (d.getElementById(id)) {return;}
+                 js = d.createElement(s); js.id = id;
+                 js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v2.10&appId=144111119374573";
+                 fjs.parentNode.insertBefore(js, fjs);
+               }(document, 'script', 'facebook-jssdk'));
+
+            window.sendToFB = function(){
+                FB.ui({
+                  method: 'feed',
+                  link: 'https://doyouaflavor.github.io/monopoly',
+                  caption: 'An example caption',
+                }, function(response){});
+                        }
+
+        $(window).on("beforeunload", function() { 
+            var index = $(Reveal.getCurrentSlide()).index();
+            ga('send', 'event', 'window', 'quit', 'slide-'+index);
+        })
+      });
+}
